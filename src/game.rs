6ba96f221084cdcfe1e8967c717ece8842fa8184
Burak Ercikti <20180808082@ogr.akdeniz.edu.tr>
2024-01-24 12:04:12 +0300

    key press setted

--- src/game.rs
+++ src/game.rs
@@ -1,10 +1,10 @@
 use piston_window::*;
-use piston_window::types::Color;
+use piston_window::types::{Color, Width};
 
 use rand::{thread_rng, Rng};
 
 use crate::snake::{Direction, Snake};
-use crate::draw::{draw_block, draw_rectangle};
+use crate::draw::{self, draw_block, draw_rectangle};
 
 const FOOD_COLOR: Color = [0.8, 0.0, 0.0, 1.0];
 const BORDER_COLOR: Color = [0.0, 0.0, 0.0, 1.0];
@@ -30,4 +30,56 @@ pub struct Game {
 
     // The time elapsed since the start of the game
     waiting_time: f64,
+}
+
+impl Game {
+    pub fn new(Width: i32, Height: i32) -> Game {
+        Game {
+            snake: Snake::new(2, 2),
+            food_exists: true,
+            food_x: 6,
+            food_y: 4,
+            width: Width,
+            height: Height,
+            game_over: false,
+            waiting_time: 0.0,
+        }
+    }
+
+    pub fn key_pressed(&mut self, key: Key) {
+        if self.game_over {
+            return;
+        }
+
+        let dir = match key {
+            Key::Up => Some(Direction::Up),
+            Key::Down => Some(Direction::Down),
+            Key::Left => Some(Direction::Left),
+            Key::Right => Some(Direction::Right),
+            _ => None,
+        };
+
+        if dir.unwrap() == self.snake.head_direction().opposite() {
+            return;
+        }
+
+        self.update_snake(dir);
+    }
+
+    pub fn draw(&self, con: &Context, g: &mut G2d) {
+        self.snake.draw(con, g);
+
+        if self.food_exists {
+            draw_block(FOOD_COLOR, self.food_x, self.food_y, con, g);
+        }
+
+        draw_rectangle(BORDER_COLOR, 0, 0, self.width, 1, con, g);
+        draw_rectangle(BORDER_COLOR, 0, self.height - 1, self.width, 1, con, g);
+        draw_rectangle(BORDER_COLOR, 0, 0, 1, self.height, con, g);
+        draw_rectangle(BORDER_COLOR, self.width - 1, 0, 1, self.height, con, g);
+
+        if self.game_over {
+            draw_rectangle(GAMEOVER_COLOR, 0, 0, self.width, self.height, con, g);
+        }
+    }
 }
